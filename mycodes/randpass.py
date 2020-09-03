eaef07add4c129f9066a622c3a339d73302ada59
Cole Sashkin <csashceh1024@gmail.com>
2020-09-02 22:26:14 -0700

    commit on 'master'

--- mycodes/randpass.py
+++ mycodes/randpass.py
@@ -1,19 +1,21 @@
 from hashlib import sha256
 from random import randint, choice, seed, randrange
 import re
+import os
 
-chars = ["a", "b", "c", "d", "e", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9]
+chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def random_pass(base_str: str="", use_seed: bool=False, seed_num: int=1, p_len: int=10, 
+def random_pass(base_str: str="", use_seed: bool=False, p_len: int=10, 
                 sym_mix: bool=False, num_sym: int=3) -> str:
     if use_seed is True:
-        seed(seed_num)
+        seed(os.urandom(2048))
     if base_str == "":
         for _ in range(16):
             base_str += str(choice(chars))
     b = int.from_bytes(bytes(base_str.encode()), "little")
-    change1 = b >> randint(1, 10)
-    change2 = b << randint(1, 10)
+    change1 = b >> randint(50, 127) 
+    change2 = b << randint(1, 49)
     changed = change1 << 2 & change2
     changed = changed % 357
     p = sha256(changed.to_bytes(8, "little")).hexdigest()
@@ -25,7 +27,10 @@ def random_pass(base_str: str="", use_seed: bool=False, seed_num: int=1, p_len: 
         s = size // 2
         p1 = p[:-s]
         p2 = p[s:]
-        p = p2 + p1
+        if (size - num_sym) % 5 and s > p_len:
+            p = p2 + choice(chars[6:26]) + p1
+        else:
+            p = p2 + p1
         p = p[:-num_sym]
         size = len(p)
     for _ in range(num_sym):
@@ -37,7 +42,10 @@ def random_pass(base_str: str="", use_seed: bool=False, seed_num: int=1, p_len: 
             p.insert(i, choice(sym))
             p = "".join(p)
     if not re.match(r"^[A-Za-z]+?", p):
-        return random_pass(base_str, use_seed, seed_num+1, 
+        return random_pass(base_str, use_seed, 
                             p_len, sym_mix, num_sym)
     else:
         return p
+
+r = random_pass(use_seed=True, sym_mix=True)
+print(r)
--- mycodes/raw_packets.py
+++ mycodes/raw_packets.py
@@ -1,14 +1,21 @@
+from collections import defaultdict
+import os
 import socket
-import sys
 import struct
+import sys
+from time import time, sleep
+from datetime import date
+
+from hex_dump import xdump
 
 
 class EthHeader:
     def __init__(self, frame):
         eth = frame[0:14]
         ehdr = struct.unpack("!6B6BH", eth)
+        self.size = struct.calcsize("!6B6BH")
         self.dst = ehdr[0:6]
-        self.src = ehdr[6:12]
+        self.src = ehdr[6:12] 
         self.etype = ehdr[12]
 
     def fmt_mac(self, data):
@@ -37,10 +44,21 @@ class EthHeader:
         print("Source: ", self.fmt_mac(self.src))
         print("Ether Type: ", "0x{:04x} {}".format(self.etype, self.get_type(self.etype)))
 
+    def __str__(self):
+        return """
+        Ethernet Header
+        ---------------------
+        Destination: {}
+        Source: {}
+        Type: 0x{:04x} {}
+        """.format(self.fmt_mac(self.dst), self.fmt_mac(self.src),
+            self.etype, self.get_type(self.etype))
+
 class IPHeader:
     def __init__(self, frame):
         hstr = frame[14:34]
         iph = struct.unpack("!BBHHHBBH4s4s", hstr)
+        self.size = struct.calcsize("!BBHHHBBH4s4s")
         self.version = iph[0] >> 4
         self.internet_header_len = (iph[0] & 0b1111) * 4
         self.type_service = iph[1] >> 6
@@ -83,11 +101,32 @@ class IPHeader:
         print(f"Source Address: {self.src_addr}")
         print(f"Destination Address: {self.dst_addr}")
 
+    def __str__(self):
+        return f"""
+        IP Header
+        ------------------
+        Version: {self.version}
+        Header Length: {self.internet_header_len}
+        Type of Service: {self.type_service}
+        Explicit Congestion Notification: {self.explicit_congestion}
+        Total Length: {self.total_len}
+        Identification: {self.id}
+        Flags: {self.flags}
+        Fragment Offset: {self.fragment_offset}
+        Time to Live: {self.time_to_live}
+        Protocol: {self.protocol}
+        Checksum: {self.checksum}
+        Source Address: {self.src_addr}
+        Destination Address: {self.dst_addr}
+        """
+
 class TCPHeader:
     def __init__(self, frame, ipheader):
         header_size = 14 + ipheader.internet_header_len
         tcphead = frame[header_size: header_size + 20]
         tcpbits = struct.unpack("!HHLLHHHH", tcphead)
+        self.raw_bits = "".join(tcpbits)
+        self.size = struct.calcsize("!HHLLHHHH")
         self.src_port = tcpbits[0]
         self.dst_port = tcpbits[1]
         self.seq_num = tcpbits[2]
@@ -132,36 +171,81 @@ class TCPHeader:
         print(f"Checksum: {self.checksum}")
         print(f"Urgent Pointer: {self.urg_pointer}")
 
+    def __str__(self):
+        return f"""
+        TCP Header
+        -------------------
+        Source Port: {self.src_port}
+        Destination Port: {self.dst_port}
+        Sequence Number: {self.seq_num}
+        Aknowledgment Number: {self.ack_num}
+        Data Offset: {self.offset}
+        Flags: {self.flags}
+        Window Size: {self.window_size}
+        Checksum: {self.checksum}
+        Urgent Pointer: {self.urg_pointer}
+        """
+
 class TCP_Packet(object):
-    packet_num = 0
     def __init__(self, frame):
         self.eth = EthHeader(frame)
         self.ip = IPHeader(frame)
         self.tcp = TCPHeader(frame, self.ip)
-        self.packet_num += 1
+        self.bytes = frame.__sizeof__() - (self.tcp.offset-1)
+        total_header_size = self.eth.size + self.ip.size + self.tcp.size
+        self.data = frame[total_header_size: total_header_size + self.tcp.offset-1]
+
     def dump(self):
-        print(f"\nTCP Packet #{self.packet_num}\n")
-        print("----------------------------------------")
+        print(f"\nTCP Packet\nbytes {self.bytes}")
+        print("----------------------------------------\n")
         self.eth.dump()
         print("\n")
         self.ip.dump()
         print("\n")
         self.tcp.dump()
+        print("\n")
 
+    def __repr__(self):
+        return f"""
+        {str(self.eth)}
+        {str(self.ip)}
+        {str(self.tcp)}
+        """
+
+
+
+arg = ""
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
 s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, 8)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+current_date = date.fromtimestamp(time())
+dumpdir = "/path/to/captures/{}".format(current_date)
+packet_by_id = defaultdict(list)
+packet_by_src_dest = defaultdict(list)
+
 try:
     while True:
         try:
             frame = s.recv(4096)
             tcp = TCP_Packet(frame)
-
+            packet_by_id[tcp.ip.id].append(repr(tcp))
+            packet_by_src_dest[(tcp.ip.src_addr, tcp.ip.dst_addr)].append(repr(tcp))
             tcp.dump()
+            xdump(tcp.tcp.raw_bits)
         except KeyboardInterrupt:
-            s.close()
-            break
-        except socket.herror:
-            s.close()
             break
 except KeyboardInterrupt:
+    s.close()
+finally:
+    if arg in {"-s"}:
+        if not os.path.exists(dumpdir):
+            os.mkdir(dumpdir)
+        for i, ps in packet_by_src_dest.items():
+            fn = dumpdir + "/{}".format(i)
+            with open(fn, "w") as fh:
+                for p in ps:
+                    fh.write(p)
+                    fh.write("\n")
     sys.exit()
